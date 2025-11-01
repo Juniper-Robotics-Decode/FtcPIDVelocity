506efe9d395593ff4b0b44e00e696de919f106c0
Alex-Robotics <alex.rud.pro@gmail.com>
2025-11-01 10:23:06 -0400

    Working velocity pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VelocityController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VelocityController.java
@@ -1,44 +1,45 @@
-﻿package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode;
 
-import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.RADIANS;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+
+import com.arcrobotics.ftclib.controller.PDController;
 import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.arcrobotics.ftclib.controller.wpilibcontroller.SimpleMotorFeedforward;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp
 @Config
-public class IntakeSpeedControl extends LinearOpMode {
+public class VelocityController extends LinearOpMode {
 
-    private Motor intakeMotor;
-    private PIDController pidController;
-    public static double p = 0.1, i = 0, d = 0.0015;
+    private MotorEx intakeMotor;
+    public static double p = 0.5, i = 0, d = 0;
     private double currentVelocity;
-    public static double targetVelocity = 0.5;
+    public static double targetVelocity = 1150;
     public static final double TICKS_PER_ROT = 145.1;
-    private double power = 0;
+    public static double kS = 0, kV = 1.055, kA = 0;
+
 
     @Override
     public void runOpMode() {
-        intakeMotor = new Motor(hardwareMap, "IM", Motor.GoBILDA.RPM_1150);
+        intakeMotor = new MotorEx(hardwareMap, "IM", Motor.GoBILDA.RPM_1150);
         intakeMotor.setRunMode(Motor.RunMode.VelocityControl);
-        pidController = new PIDController(p, i, d);
         this.telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         waitForStart();
         while (opModeIsActive()) {
-            pidController.setPID(p, i, d);
-            currentVelocity = intakeMotor.get();
-            power += pidController.calculate(currentVelocity, targetVelocity);
 
-            intakeMotor.set(power);
+            currentVelocity = intakeMotor.getVelocity();
+            intakeMotor.setFeedforwardCoefficients(kS, kV, kA);
+            intakeMotor.setVeloCoefficients(p, i, d);
+            intakeMotor.setVelocity(targetVelocity);
+
 
             telemetry.addData("Current Velocity (rot/sec): ", currentVelocity);
             telemetry.addData("Target Velocity (rot/sec): ", targetVelocity);
@@ -46,23 +47,4 @@ public class IntakeSpeedControl extends LinearOpMode {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
